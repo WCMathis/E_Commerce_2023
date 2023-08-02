acbc92a684768756b42abb61c56b15c5720a1f40
WCMathis <williammathis64@gmail.com>
2023-08-02 04:59:23 +0100

    video 18 complete

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=250, db_index =True)
-    slug = models.models.SlugField(max_length=250, unique = True)
+    slug = models.SlugField(max_length=250, unique = True)
 
     class Meta:
         verbose_name_plural = 'categories'
@@ -18,7 +18,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     slug = models.SlugField(max_length=250)
     price = models.DecimalField(max_digits=4, decimal_places=2)
-    #image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/')
 
     class Meta:
         verbose_name_plural = 'products'
